ea27779b47e30332602eaf12d09977bb184989c1
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-05-16 22:30:58 +0300

    added router for registration

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
+import UserModel from './models/User.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
@@ -12,6 +17,47 @@ mongoose
 
 app.get('/', (req, res) => {
     res.send('Working');
+})
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        const doc = new UserModel({
+            fullName: req.body.fullName,
+            email: req.body.email,
+            passwordHash: hash,
+        });
+
+        const user = await doc.save();
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            process.env.ID_TOKEN,
+            {
+                expiresIn: '30d'
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+        res.json({
+            ...userData,
+            token
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Не вдалося зареєструватися',
+        })
+    }
 })
 
 app.listen(port, (err) => {
