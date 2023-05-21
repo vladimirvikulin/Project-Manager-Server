2d3ad949eda95c677bb9ca7f257aa1d920b7e099
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-05-22 01:16:07 +0300

    fixed bug with no token in request

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1] || '';
+    const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.ID_KEY);
